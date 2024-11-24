0d8f6c30f8c28ebe8bda6629193c16692e297984
Mohamed Abdelmoety <167744542+mohamed-h237@users.noreply.github.com>
2024-11-24 15:56:20 +0300

    title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/Home";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    // Update the tab title
+    document.title = "Nasah - Your Trusted Legal Platform";
+
+    // Update the favicon
+    const favicon = document.querySelector("link[rel='icon']");
+    if (favicon) {
+      favicon.setAttribute("href", "https://naseh.qa/assets/naseh-logo.CpTfn_N7.svg");
+    } else {
+      const newFavicon = document.createElement("link");
+      newFavicon.rel = "icon";
+      newFavicon.href = "https://naseh.qa/assets/naseh-logo.CpTfn_N7.svg";
+      document.head.appendChild(newFavicon);
+    }
+  }, []);
+
   return <Home />;
 };
 
